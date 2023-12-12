7c72d2e16ede261c22379dc661953fad90666f00
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-12-12 13:18:42 +0100

    fetch from api

--- bridgeproblem_frontend/src/app/page.tsx
+++ bridgeproblem_frontend/src/app/page.tsx
@@ -1,19 +1,71 @@
 "use client";
-// Home.tsx
-import React from "react";
-import ImageComponent from "@/components/ImageComponent";
-import RedCar from "@/assets/redCar.png";
+import React, { useState, useEffect } from "react";
+
+interface Car {
+  name: string;
+  state: string;
+  source: string;
+  processingTime: number;
+  position: number;
+}
 
 const Home: React.FC = () => {
+  const [carData, setCarData] = useState<Car[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api");
+        const data = await response.json();
+        setCarData(data);
+        setLoading(false);
+      } catch (error) {
+        setError("Error fetching data");
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  const waitingSouthCars = carData.filter(
+    (car) => car.state === "WAITING" && car.source === "SOUTH"
+  );
+
+  const waitingNorthCars = carData.filter(
+    (car) => car.state === "WAITING" && car.source === "NORTH"
+  );
+
+  const processingCars = carData.filter((car) => car.state === "PROCESSING");
+
+  const processedSouthCars = carData.filter(
+    (car) => car.state === "PROCESSED" && car.source === "SOUTH"
+  );
+
+  const processedNorthCars = carData.filter(
+    (car) => car.state === "PROCESSED" && car.source === "NORTH"
+  );
+
+  console.log("waitingSouthCars", waitingSouthCars);
+  console.log("waitingNorthCars", waitingNorthCars);
+  console.log("processingCars", processingCars);
+  console.log("processedSouthCars", processedSouthCars);
+  console.log("processedNorthCars", processedNorthCars);
+
   return (
     <div style={{ position: "relative", height: "400px", width: "600px" }}>
       <h1>O kurwa to działa</h1>
-      <ImageComponent
-        imagePath={RedCar}
-        position={{ top: 100, left: 100, rotation: 90 }}
-        size={{ width: 150, height: 100 }} // Specify width and height
-        speed={2}
-      />
     </div>
   );
 };
