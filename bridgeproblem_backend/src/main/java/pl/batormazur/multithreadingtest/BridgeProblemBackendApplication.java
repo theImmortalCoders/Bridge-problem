25668aa4761acb14f750a9ab79d220ed086b982a
marcinbator <marcinbator.ofc@gmail.com>
2023-12-13 22:52:33 +0100

    unit tests

--- bridgeproblem_backend/src/main/java/pl/batormazur/multithreadingtest/BridgeProblemBackendApplication.java
+++ bridgeproblem_backend/src/main/java/pl/batormazur/multithreadingtest/BridgeProblemBackendApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultithreadingTestApplication {
+public class BridgeProblemBackendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MultithreadingTestApplication.class, args);
+        SpringApplication.run(BridgeProblemBackendApplication.class, args);
     }
 }
