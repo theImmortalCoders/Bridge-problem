66402cd78b70a42c5c74510e6d7f8ea54d2c18d4
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-12-14 10:25:37 +0100

    line draw

--- bridgeproblem_frontend/src/app/page.tsx
+++ bridgeproblem_frontend/src/app/page.tsx
@@ -46,75 +46,138 @@ const Home: React.FC = () => {
   };
 
   return (
-    <div className="grid grid-cols-3 gap-4 h-[20vh] w-[100vw]">
-      <div className="col-span-1 p-4">
-        <h1>waitingNorthCars {waitingNorthCars.length}</h1>
-        <h1>processedNorthCars {processedNorthCars.length}</h1>
-        <button onClick={handleAddCarNorth}>Dodaj car NORTH</button>
-      </div>
-      <div className="col-span-1 p-4">
-        <h1>processingCars {processingCars.length}</h1>
-        {travelTime !== null ? (
-          <h1>travelTime {travelTime}</h1>
-        ) : (
-          <h1>travelTime = 0</h1>
-        )}
-        current direction:
-        {currentDirection}
-        max cars amount:
-        {maxCarsAmount}
-        <button onClick={handleIncreaseSetMaxCars}>
-          Zwiększ max. liczbę aut z jednej strony naraz
-        </button>
-        <button onClick={handleDecreaseSetMaxCars}>
-          Zmniejsz max. liczbę aut z jednej strony naraz
-        </button>
-      </div>
-      <div className="col-span-1 p-4">
-        <h1>waitingSouthCars {waitingSouthCars.length}</h1>
-        <h1>processedSouthCars {processedSouthCars.length}</h1>
-        <button onClick={handleAddCarSouth}>Dodaj car SOUTH</button>
-      </div>
-      <div className="grid grid-cols-[40vw,20vw,40vw] gap-4 h-[20vh] w-[100vw] justify-center">
-        <div className="bg-orange-800 flex flex-row-reverse items-center">
-          {waitingNorthCars.map((car, index) => (
-            <p
-              key={index}
-              className="rotate-90 flex flex-col justify-center items-center"
-            >
-              {`Travel Time: ${car.processingTime}`}
-              <Image src={"/redCar.png"} alt={""} width={100} height={100} />
-            </p>
-          ))}
+    <div className="min-h-[100vh] min-w-[100vw]">
+      <div className="grid grid-cols-3 gap-4 w-[100vw]">
+        <div className="col-span-1 p-4">
+          <h1>waitingNorthCars {waitingNorthCars.length}</h1>
+          <h1>processedNorthCars {processedNorthCars.length}</h1>
+          <button onClick={handleAddCarNorth}>Dodaj car NORTH</button>
         </div>
-        <div className="bg-orange-800 flex flex-row items-center justify-center">
-          {processingCars.map((car, index) => (
-            <p
-              key={index}
-              className={`${
-                car.source === "NORTH" ? "" : "-"
-              }rotate-90 flex flex-col justify-center items-center`}
-            >
-              {`Travel Time: ${car.processingTime}`}
-              <Image
-                src={`/${car.source === "NORTH" ? "red" : "blue"}Car.png`}
-                alt={""}
-                width={100}
-                height={100}
-              />
-            </p>
-          ))}
+        <div className="col-span-1 p-4">
+          <h1>processingCars {processingCars.length}</h1>
+          {travelTime !== null ? (
+            <h1>travelTime {travelTime}</h1>
+          ) : (
+            <h1>travelTime = 0</h1>
+          )}
+          current direction:
+          {currentDirection}
+          max cars amount:
+          {maxCarsAmount}
+          <button onClick={handleIncreaseSetMaxCars}>
+            Zwiększ max. liczbę aut z jednej strony naraz
+          </button>
+          <button onClick={handleDecreaseSetMaxCars}>
+            Zmniejsz max. liczbę aut z jednej strony naraz
+          </button>
         </div>
-        <div className="bg-orange-800 flex flex-row items-center">
-          {waitingSouthCars.map((car, index) => (
-            <p
-              key={index}
-              className="-rotate-90 flex flex-col justify-center items-center"
-            >
-              {`Travel Time: ${car.processingTime}`}
-              <Image src={"/blueCar.png"} alt={""} width={100} height={100} />
-            </p>
-          ))}
+        <div className="col-span-1 p-4">
+          <h1>waitingSouthCars {waitingSouthCars.length}</h1>
+          <h1>processedSouthCars {processedSouthCars.length}</h1>
+          <button onClick={handleAddCarSouth}>Dodaj car SOUTH</button>
+        </div>
+      </div>
+
+      {/* TUTAJ JEST JEZDNIA */}
+      <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-[40vw,20vw,40vw] gap-4 w-[100vw] justify-center">
+          <div className="flex flex-col items-center">
+            <div className="bg-yellow-300 h-[20vh] w-full mb-4 flex flex-row items-center justify-start">
+              {processedNorthCars.slice(-3).map((car, index) => (
+                <p
+                  key={index}
+                  className="flex flex-col justify-center items-center px-1"
+                >
+                  <Image
+                    src={"/blueCar.png"}
+                    alt={""}
+                    width={100}
+                    height={100}
+                    className="-rotate-90"
+                  />
+                  {`Travel Time: ${car.processingTime / 1000}`}
+                </p>
+              ))}
+            </div>
+            <div className="bg-yellow-300 h-[20vh] w-full flex flex-row items-center justify-end">
+              {waitingNorthCars.map((car, index) => (
+                <p
+                  key={index}
+                  className="flex flex-col justify-center items-center px-1"
+                >
+                  <Image
+                    src={"/redCar.png"}
+                    alt={""}
+                    width={100}
+                    height={100}
+                    className="rotate-90"
+                  />
+                  {`Travel Time: ${car.processingTime / 1000}`}
+                </p>
+              ))}
+            </div>
+          </div>
+          <div className="flex items-center justify-center h-full w-full">
+            <div className="bg-yellow-300 flex items-center justify-center h-[20vh] w-full">
+              {processingCars.map((car, index) => (
+                <p
+                  key={index}
+                  className={`${
+                    car.source === "NORTH" ? "" : "-"
+                  }flex flex-col justify-center items-center`}
+                >
+                  <Image
+                    src={`/${car.source === "NORTH" ? "red" : "blue"}Car.png`}
+                    alt={""}
+                    width={100}
+                    height={100}
+                    style={{
+                      transform: `rotate(${
+                        car.source === "NORTH" ? 90 : -90
+                      }deg)`,
+                    }}
+                  />
+                  {`Travel Time: ${car.processingTime / 1000}`}
+                </p>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="bg-yellow-300 h-[20vh] w-full mb-4 flex flex-row items-center justify-end">
+              {processedSouthCars.slice(-3).map((car, index) => (
+                <p
+                  key={index}
+                  className="flex flex-col justify-center items-center px-1"
+                >
+                  <Image
+                    src={"/redCar.png"}
+                    alt={""}
+                    width={100}
+                    height={100}
+                    className="rotate-90"
+                  />
+                  {`Travel Time: ${car.processingTime / 1000}`}
+                </p>
+              ))}
+            </div>
+            <div className="bg-yellow-300 h-[20vh] w-full flex flex-row items-center justify-start">
+              {waitingSouthCars.map((car, index) => (
+                <p
+                  key={index}
+                  className=" flex flex-col justify-center items-center px-1"
+                >
+                  <Image
+                    src={"/blueCar.png"}
+                    alt={""}
+                    width={100}
+                    height={100}
+                    className="-rotate-90"
+                  />
+                  {`Travel Time: ${car.processingTime / 1000}`}
+                </p>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
